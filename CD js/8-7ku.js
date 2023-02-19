e148ad3151370ae97f850b2cad63b20858effdce
Andrew Marcker <ticey1995@gmail.com>
2023-02-19 16:18:28 +0200

    four

--- CD js/8-7ku.js
+++ CD js/8-7ku.js
@@ -17,14 +17,23 @@ function countSheeps(arrayOfSheep) {
 //n3
 function checkExam(array1, array2) {
     let result = 0
-    for(let i = 0;i < array1.length;i++){
-    if(array2[i] === ""){
-    result += 0
-    } else if(array1[i] === array2[i]){
-    result += 4
-    } else if (array1[i] !== array2[i]){
-    result -= 1
+    for (let i = 0; i < array1.length; i++) {
+        if (array2[i] === "") {
+            result += 0
+        } else if (array1[i] === array2[i]) {
+            result += 4
+        } else if (array1[i] !== array2[i]) {
+            result -= 1
+        }
     }
+    return result <= 0 ? 0 : result
 }
-return result <= 0?0:result
+//n4
+function friend(friends) {
+    let arr = [];
+    for (let i = 0; i < friends.length; i++) {
+        if (friends[i].length == 4) {
+            arr.push(friends[i])
+        }
+    } return arr
 }
